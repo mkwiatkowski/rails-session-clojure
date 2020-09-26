cf4eedccd10049311540f0aefe616f504757f0ba
Łukasz Korecki <lukasz@korecki.me>
2020-09-26 15:48:05 -0700

    Update version

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject lukaszkorecki/rails-session-clojure "0.2.0"
-  :description "Integration with Ruby on Rails encoded and signed session cookies"
+(defproject lukaszkorecki/rails-session-clojure "0.3.0-SNAPSHOT"
+  :description "Integration with Ruby on Rails encoded and signed session cookies. Maintained fork"
   :url "https://github.com/lukaszkorecki/rails-session-clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]]
   :global-vars {*warn-on-reflection* true}
   :dependencies [[cheshire "5.10.0"]
                  [crypto-equality "1.0.0"]
